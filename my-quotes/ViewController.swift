93b1934f790378afa8ee1c9e66e61c7f8a0a0457
Daniel Avram <dani.avram91@gmail.com>
2017-03-09 11:46:46 +0200

    Initial Commit

---
+++ my-quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-quotes
+//
+//  Created by Daniel Avram on 09/03/2017.
+//  Copyright © 2017 Daniel Avram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
